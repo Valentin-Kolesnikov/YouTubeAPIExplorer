c268226189cf348ac6cd95b03fc841c164754005
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2025-09-07 18:15:58 +0300

    Uninterrupted operation is provided

--- Sources/CommentExplorer.py
+++ Sources/CommentExplorer.py
@@ -1,6 +1,8 @@
 # from collections import Counter
 # import pandas as pd
+from googleapiclient.errors import HttpError
 import re
+import requests
 
 def youtube_id_finder(url):
     while True:
@@ -10,6 +12,33 @@ def youtube_id_finder(url):
             return match.group(1)
         else:
             url = input("\nTry entering the url again: ")
+
+def channel_name(video_id, api_key):
+    try:
+        name = requests.get(
+            "https://www.googleapis.com/youtube/v3/videos",
+            params={"part": "snippet", "id": video_id, "key": api_key}
+        )
+        return name.json()["items"][0]["snippet"]["channelTitle"], False
+    
+    except HttpError as exc:
+        status = exc.resp.status
+
+        if status == 400:
+            print(f"\n\u001b[31mError {status}: Bad Request. There is some issues with Google requests.\u001b[0m")
+
+        elif status == 403:
+            print(f"\n\u001b[31mError {status}: Forbidden. Probably, you exceeded your YouTube API quota.\u001b[0m")
+
+        elif status == 404:
+            print(f"\n\u001b[31mError {status}: Not Found. Probably, the non-existent video was found.\u001b[0m")
+
+        else:
+            print(f"\n\u001b[31mUnexpected HTTP error: {status}\u001b[0m")
+        
+        input("\nPress Enter to return...")
+
+        return {}, True
 
 def youtube_which_order():
     which_order = input("\nDo you need to sort comments? By relevance - 1; By time - 2: ")
@@ -27,40 +56,60 @@ def collect_comments(video_id, search_terms, which_order, youtube):
     comments = []
     next_page_token = None
 
-    while True:
-        request = youtube.commentThreads().list(
-            part= "snippet,replies",
-            videoId= video_id,
-            pageToken= next_page_token,
-            maxResults= 50,
-            textFormat= "plainText",
-            order= which_order,
-        ).execute()
+    try:
+        while True:
+            request = youtube.commentThreads().list(
+                part= "snippet,replies",
+                videoId= video_id,
+                pageToken= next_page_token,
+                maxResults= 50,
+                textFormat= "plainText",
+                order= which_order,
+            ).execute()
 
-        for item in request["items"]:
-            comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-            if any(search_term.lower() in comment.lower() for search_term in search_terms):
-                comments.append(comment)
-            elif not search_terms:
-                comments.append(comment)
-                continue 
+            for item in request["items"]:
+                comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+                if any(search_term.lower() in comment.lower() for search_term in search_terms):
+                    comments.append(comment)
+                elif not search_terms:
+                    comments.append(comment)
+                    continue 
 
-            if "replies" in item:
-                for reply in item["replies"]["comments"]:
-                    reply_comments = reply["snippet"]["textDisplay"]
-                    if any(reply_term.lower() in reply_comments.lower() for reply_term in search_terms):
-                        comments.append(reply_comments)
-                    elif not search_terms:
-                        comments.append(reply_comments)
-                        continue
+                if "replies" in item:
+                    for reply in item["replies"]["comments"]:
+                        reply_comments = reply["snippet"]["textDisplay"]
+                        if any(reply_term.lower() in reply_comments.lower() for reply_term in search_terms):
+                            comments.append(reply_comments)
+                        elif not search_terms:
+                            comments.append(reply_comments)
+                            continue
 
-        comments = list(set(comments))
+            comments = list(set(comments))
 
-        next_page_token = request.get("nextPageToken")
-        if not next_page_token:
-            break
+            next_page_token = request.get("nextPageToken")
+            if not next_page_token:
+                break
 
-    return comments
+        return comments, False
+    
+    except HttpError as exc:
+        status = exc.resp.status
+
+        if status == 400:
+            print(f"\n\u001b[31mError {status}: Bad Request. There is some issues with Google requests.\u001b[0m")
+
+        elif status == 403:
+            print(f"\n\u001b[31mError {status}: Forbidden. Probably, comments of the video are disabled or you exceeded your YouTube API quota.\u001b[0m")
+
+        elif status == 404:
+            print(f"\n\u001b[31mError {status}: Not Found. Probably, the non-existent video was found.\u001b[0m")
+
+        else:
+            print(f"\n\u001b[31mUnexpected HTTP error: {status}\u001b[0m")
+        
+        input("\nPress Enter to return...")
+
+        return {}, True
 
 def numberofcomments(comments, number, channel):
     print(f"Channel: {channel}")
--- Sources/YouTubeExplorer.py
+++ Sources/YouTubeExplorer.py
@@ -1,18 +1,24 @@
 from CommentExplorer import *
-from ChannelExplorer import channel_name
+# from ChannelExplorer import *
 from VideoExplorer import *
-from time import sleep
 from Key import youtube_api_key
+from TestQuota import test_quota
+from time import sleep
+import asyncio
+
 import os
 
 def launcherComments(youtube):
-    sleep(1)
+    sleep(0.6)
     url = input("\nEnter the url: ")     
     video_id = youtube_id_finder(url)
                                                 
-    channel = channel_name(video_id, api_key)
+    channel, exc = channel_name(video_id, api_key)
+    if exc:
+        os.system('cls')
+        return
 
-    sleep(1)
+    sleep(0.6)
     search_terms = []
     while True:
         terms = input("\nEnter the keywords by one (press Enter to continue): ")
@@ -22,64 +28,95 @@ def launcherComments(youtube):
 
     search_terms = set(search_terms)
 
-    sleep(1)
+    sleep(0.6)
     which_order = youtube_which_order()
 
-    comments = collect_comments(video_id, search_terms, which_order, youtube)
+    comments, exc = collect_comments(video_id, search_terms, which_order, youtube)
+    if exc:
+        os.system('cls')
+        return
 
     count_keys(comments, search_terms)
 
-    sleep(1)
+    sleep(0.6)
     number = int(input("\nHow many comments do you need?: "))
     os.system('cls')
     numberofcomments(comments, number, channel)
 
-    input("\nPress Enter to exit...")
+    input("\nPress Enter to continue...")
+
 
 def launcherVideos(youtube):
-    sleep(1)
+    sleep(0.6)
     keywords = input("\nEnter a request on YouTube: ")
 
-    sleep(1)
+    sleep(0.6)
     region = input("\nWhat region would you like? (Enter as US, RU, UK, etc): ")
     region.upper()
+    while True:
+        if len(region) == 2 and region.isalpha():
+            break
+        else:
+            region = input("\nEnter it again: ")
 
     age, duration = searching_for_videos()
 
-    sleep(1)
+    sleep(0.6)
     maximum = int(input("\nHow much do you want to receive videos?: "))
     if maximum > 51:
         maximum = 50
-    elif maximum < 0:
+    elif maximum < 4:
         maximum = 5
-    sleep(2)
+    sleep(1.2)
     os.system('cls')
 
-    video_ids, channel_ids = collect_searches(youtube, keywords, region, age, duration, maximum)
+    video_ids, channel_ids, exc = collect_searches(youtube, keywords, region, age, duration, maximum)
+    if exc:
+        os.system('cls')
+        return
 
-    results = ryd(video_ids)
+    results = asyncio.run(ryd(video_ids))
 
-    statrequest, dict_channels = collect_stats(youtube, video_ids, channel_ids)
+    statrequest, dict_channels, exc = collect_stats(youtube, video_ids, channel_ids)
+    if exc:
+        os.system('cls')
+        return
 
     output_videos(results, statrequest, dict_channels)
 
-    input("\nPress Enter to exit...")
+    input("Press Enter to continue...")
+
     
 def launcherChannels(youtube):
 
-    input("\nPress Enter to exit...")
+    input("Press Enter to continue...")
 
 if __name__ == "__main__":
     youtube, api_key = youtube_api_key()
-    os.system('cls')
     print("Key is accepted!")
-    sleep(1)
-    print("\nWhat do you need to explore?")
-    sleep(1)
-    question = int(input("Comments - 1; Videos - 2; Channels - 3: "))
-    if question == 1:
-        launcherComments(youtube)
-    elif question == 2:
-        launcherVideos(youtube)
-    elif question == 3:
-        launcherChannels(youtube)
+    sleep(0.6)
+
+    if not test_quota(youtube):
+        input("Press Enter to exit...")
+        exit(1)
+
+    while True:
+        os.system('cls')
+        print("What do you need to explore?")
+        sleep(0.6)
+
+        questionist = input("Comments - 1; Videos - 2; Channels - 3; Exit - 0: ")
+        while True:
+            if questionist == '1':
+                launcherComments(youtube)
+                break
+            elif questionist == '2':
+                launcherVideos(youtube)
+                break
+            elif questionist == '3':
+                launcherChannels(youtube)
+                break
+            elif questionist == '0':
+                exit(0)
+            else:
+                questionist = input("Enter it again: ")
