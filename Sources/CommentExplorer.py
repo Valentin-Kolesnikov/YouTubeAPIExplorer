c92cefb6a3a6dea76195c76615c85d75376555d4
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2025-09-08 21:30:26 +0300

    Total comments is ready

--- Sources/CommentExplorer.py
+++ Sources/CommentExplorer.py
@@ -3,6 +3,7 @@
 from googleapiclient.errors import HttpError
 import re
 import requests
+import json
 
 def youtube_id_finder(url):
     while True:
@@ -93,13 +94,21 @@ def collect_comments(video_id, search_terms, which_order, youtube):
         return comments, False
     
     except HttpError as exc:
+        
         status = exc.resp.status
 
         if status == 400:
             print(f"\n\u001b[31mError {status}: Bad Request. There is some issues with Google requests.\u001b[0m")
 
         elif status == 403:
-            print(f"\n\u001b[31mError {status}: Forbidden. Probably, comments of the video are disabled or you exceeded your YouTube API quota.\u001b[0m")
+            error_reason = exc.content.decode("utf-8")
+            error_json = json.loads(error_reason)
+            reason = error_json["error"]["errors"][0]["reason"]
+
+            if reason == "commentsDisabled":
+                    print(f"\n\u001b[31mError {status}: Forbidden. Comments of the video are disabled.\u001b[0m")
+            else:
+                print(f"\n\u001b[31mError {status}: Forbidden. Probably, you exceeded your YouTube API quota.\u001b[0m")
 
         elif status == 404:
             print(f"\n\u001b[31mError {status}: Not Found. Probably, the non-existent video was found.\u001b[0m")
@@ -111,16 +120,19 @@ def collect_comments(video_id, search_terms, which_order, youtube):
 
         return {}, True
 
+def count_keys(comments, search_terms):
+    counts = {kw: 0 for kw in search_terms}
+    amount_comments = 0
+    for comment in comments:
+        amount_comments += 1
+        for kw in search_terms:
+            counts[kw] += comment.lower().count(kw.lower())
+
+    print(f"Total comments: {amount_comments}")
+    for kw, count in counts.items():
+        print(f"{kw}: {count}")
+    
 def numberofcomments(comments, number, channel):
     print(f"Channel: {channel}")
     for i, c in enumerate(comments[:number], 1):
         print(f"\n\n{i}:\n{c}")
-
-def count_keys(comments, search_terms):
-    counts = {kw: 0 for kw in search_terms}
-    for comment in comments:
-        for kw in search_terms:
-            counts[kw] += comment.lower().count(kw.lower())
-
-    for kw, count in counts.items():
-        print(f"{kw}: {count}")
