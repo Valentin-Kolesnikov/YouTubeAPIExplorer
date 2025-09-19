f2a44e63755d84ec3728da0fe934132c105c1713
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2025-09-19 13:02:47 +0300

    The patterns are enabled

--- Sources/SecondFunctions/collecting_info.py
+++ Sources/SecondFunctions/collecting_info.py
@@ -1,6 +1,5 @@
 from googleapiclient.errors import HttpError
-import asyncio
-import aiohttp
+from Patterns.httperror import http_error
 
 def collect_searches(youtube, keywords, region, ageAfter, ageBefore, duration, maximum, which_order, dimension):
     try:
@@ -30,41 +29,10 @@ def collect_searches(youtube, keywords, region, ageAfter, ageBefore, duration, m
         return video_ids, channel_ids, False
     
     except HttpError as exc:
-        status = exc.resp.status
-
-        if status == 400:
-            print(f"\n\u001b[31mError {status}: Bad Request. There is some issues with Google requests.\u001b[0m")
-
-        elif status == 403:
-            print(f"\n\u001b[31mError {status}: Forbidden. Probably, you exceeded your YouTube API quota.\u001b[0m")
-
-        elif status == 404:
-            print(f"\n\u001b[31mError {status}: Not Found. Probably, the non-existent video was found.\u001b[0m")
-
-        else:
-            print(f"\n\u001b[31mUnexpected HTTP error: {status}\u001b[0m")
         
-        input("\nPress Enter to return...")
+        http_error(exc)
         
         return {}, {}, True
-
-
-async def fetch_ryd(session, vid):
-    ryd_url = f"https://returnyoutubedislikeapi.com/votes?videoId={vid}"
-    async with session.get(ryd_url) as dislike:
-        if dislike.status == 200:
-            return vid, await dislike.json()
-        else:
-            return vid, {"error": "N/A"}
-        
-async def ryd(video_ids):
-    results = {}
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch_ryd(session, vid) for vid in video_ids]
-        responses = await asyncio.gather(*tasks)
-        for vid, data in responses:
-            results[vid] = data
-    return results
 
 
 def collect_stats(youtube, video_ids, channel_ids):
@@ -89,21 +57,8 @@ def collect_stats(youtube, video_ids, channel_ids):
         return statrequest, dict_channels, False
     
     except HttpError as exc:
-        status = exc.resp.status
 
-        if status == 400:
-            print(f"\n\u001b[31mError {status}: Bad Request. There is some issues with Google requests.\u001b[0m")
-
-        elif status == 403:
-            print(f"\n\u001b[31mError {status}: Forbidden. Probably, you exceeded your YouTube API quota.\u001b[0m")
-
-        elif status == 404:
-            print(f"\n\u001b[31mError {status}: Not Found. Probably, the non-existent video was found.\u001b[0m")
-
-        else:
-            print(f"\n\u001b[31mUnexpected HTTP error: {status}\u001b[0m")
-        
-        input("\nPress Enter to return...")
+        http_error(exc)
 
         return {}, {}, True
     
