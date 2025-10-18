06a2d2d28af30aeb3fde8f9001f9b8680c5c6e81
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2025-10-19 00:36:33 +0300

    KeyboardInterrupt turned into a reboot by Ctrl+C

--- Sources/YouTubeExplorer.py
+++ Sources/YouTubeExplorer.py
@@ -23,38 +23,43 @@ import os
 
 
 if __name__ == "__main__":
-    window_title("YouTube Explorer")
-
-    youtube, exc = youtube_OAuth2()
-    if exc:
-        input("\nYou need OAuth 2 to use some points in Playlists.\n\nPress Enter to continue...")
-
-        youtube = youtube_api_key()
-
-    if not test_quota(youtube):
-        input("Press Enter to exit...")
-        exit(1)
-
     while True:
-        os.system('cls')
-        print("=========  v.0.8.0  =========")
-        print("1. Comments\n2. Videos\n3. Channels\n4. Playlists\n0. Exit")
+        try:
+            window_title("YouTube Explorer")
 
-        questionist = input("What do you need to explore?: ") 
-        while True:
-            if questionist == '1':
-                launcherComments(youtube)
-                break
-            elif questionist == '2':
-                launcherVideos(youtube)
-                break
-            elif questionist == '3':
-                launcherChannels(youtube)
-                break
-            elif questionist == '4':
-                launcherPlaylists(youtube)
-                break
-            elif questionist == '0':
-                exit(0)
-            else:
-                questionist = input("\nEnter again: ")
+            youtube, exc = youtube_OAuth2()
+            if exc:
+                input("\nYou need OAuth 2 to use some points in Playlists.\n\nPress Enter to continue...")
+
+                youtube = youtube_api_key()
+
+            if not test_quota(youtube):
+                input("Press Enter to exit...")
+                exit(1)
+
+            while True:
+                os.system('cls')
+                print("=========  v.0.9.0  =========")
+                print("1. Comments\n2. Videos\n3. Channels\n4. Playlists\n0. Exit")
+
+                questionist = input("What do you need to explore?: ") 
+                while True:
+                    if questionist == '1':
+                        launcherComments(youtube)
+                        break
+                    elif questionist == '2':
+                        launcherVideos(youtube)
+                        break
+                    elif questionist == '3':
+                        launcherChannels(youtube)
+                        break
+                    elif questionist == '4':
+                        launcherPlaylists(youtube)
+                        break
+                    elif questionist == '0':
+                        exit(0)
+                    else:
+                        questionist = input("\nEnter again: ")
+
+        except KeyboardInterrupt:
+            pass
